b9a7be6648d4dbe7b1e8a960ea3c5def192b4d86
kaue-santos <kauemurilodossantos@gmail.com>
2025-11-10 23:28:19 -0300

    feat: aula 04 pt 3

--- src/main/java/com/admin/catalogo/infrastructure/Aula04Operadores.java
+++ src/main/java/com/admin/catalogo/infrastructure/Aula04Operadores.java
@@ -8,5 +8,25 @@ public class Aula04Operadores {
 
         System.out.println(numero1-numero2);
 
+        // %
+
+        int resto = 20 % 2;
+
+        System.out.println(resto);
+
+        // < > <= >= == !=
+
+        boolean isDezMaiorQueVinte = 10 > 20;
+
+        System.out.println(isDezMaiorQueVinte);
+
+        // && || !
+
+        int idade = 20;
+        float salario = 10f;
+
+        boolean isDentroDaLei = idade > 18 && salario > 30;
+
+        System.out.println(isDentroDaLei);
     }
 }
